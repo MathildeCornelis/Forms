d72303de04e19af60d0bb62d0bb4b25d95c483a2
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-09 15:25:55 +0100

    ex 3

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
     let secondInput = document.getElementById('age');
     let firstSection = document.getElementById('a-hard-truth');
     /*ajouter un événement à l'id de l'input, quand entre un âge, on exécute une fonction*/
-    secondInput.addEventListener('keyup', addTruth)
+    secondInput.addEventListener('keyup', addTruth);
     /*fonction*/
     function addTruth() {
         /*si l'âge est plus petit ou inferieur à 17 -> hidden*/
@@ -30,3 +30,35 @@
             firstSection.style.visibility = "visible";
         }
     };
+
+/*Well this is a common one. Add a keyup listener on both fields and show a visual hint (for instance turn the field red) if the password is too short (less than 6 characters) or if the password and its confirmation do not match.*/
+
+/*renprendre les id des input qui se trouve dans le html */
+let firstPwd = document.getElementById('pwd');
+let secondPwd = document.getElementById('pwd-confirm');
+/*ajouter un événement à l'id de l'input pwd-confirm, quand entre une valeur a pwd, on exécute une fonction*/
+secondPwd.addEventListener('keyup', addPwd);
+/*fonction*/
+function addPwd() {
+    /*si pwd égale pas pwd-confirm alors bg-color = red*/
+    if (firstPwd.value != secondPwd.value) {
+        secondPwd.style.background = 'red';
+    } 
+    /*si pwd égale pwd-confirm alors bg-color = red*/
+    else {
+        secondPwd.style.background = 'green';
+    }
+};
+/*ajouter un événement à l'id de l'input pwd, quand entre une valeur a pwd, on exécute une fonction*/
+firstPwd.addEventListener('keyup', lengthPwd);
+/*fonction*/
+function lengthPwd() {
+    /*si la longeur de la valeur de pdw est inferieure ou égale à 6, alors bgcolor = red*/
+    if (firstPwd.value.length <= 6) {
+        firstPwd.style.background = 'red'
+    }
+    /*si la longeur de la valeur de pdw est supperieur à 6, alors bgcolor = red*/
+    else {
+        firstPwd.style.background = 'green'
+    }
+};
