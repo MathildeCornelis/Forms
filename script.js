f4ada96e67460570e836436643823d8185a65783
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-09 14:27:24 +0100

    ex 2

--- script.js
+++ script.js
@@ -11,3 +11,22 @@
         /*aller rechercher le span via son id dans le document et écrire dans l'html la valeur de let x*/
         document.getElementById("display-firstname").innerHTML = x;
     };
+
+/*Add a keyup listener on the second input (the number input), so that if the age is below 18 the content of the section a-hard-truth remains hidden, otherwise show them this hard to swallow fact.*/
+
+    /*renprendre les id du input et de la section qui se trouve dans le html */
+    let secondInput = document.getElementById('age');
+    let firstSection = document.getElementById('a-hard-truth');
+    /*ajouter un événement à l'id de l'input, quand entre un âge, on exécute une fonction*/
+    secondInput.addEventListener('keyup', addTruth)
+    /*fonction*/
+    function addTruth() {
+        /*si l'âge est plus petit ou inferieur à 17 -> hidden*/
+        if (secondInput.value <= 17) {
+            firstSection.style.visibility = "hidden";
+        }
+        /*si l'âge est suppérieur à 18 -> visible*/
+        else{
+            firstSection.style.visibility = "visible";
+        }
+    };
